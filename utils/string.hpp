3492d210127cd3d2e14f857c539e88e65b0ae14b
Lyrex <admin@lyrex.net>
2014-10-29 14:26:06 +0100

    fixed linux support
    Signed-off-by: Lyrex <admin@lyrex.net>

--- utils/string.hpp
+++ utils/string.hpp
@@ -17,17 +17,15 @@ namespace thunder
 				std::vector<std::string> result;
 
 				if ( start_marker.empty() || end_marker.empty() )
-				{
 					return std::vector<std::string>();
-				}
 
 				std::string::size_type start_found = 0;
 				std::string::size_type end_found = 0;
 
 				if ( ignore_case )
 				{
-					std::transform( start_marker.begin(), start_marker.end(), start_marker.begin(), std::tolower );
-					std::transform( end_marker.begin(), end_marker.end(), end_marker.begin(), std::tolower );
+					std::transform( start_marker.begin(), start_marker.end(), start_marker.begin(), ::tolower );
+					std::transform( end_marker.begin(), end_marker.end(), end_marker.begin(), ::tolower );
 				}
 
 				do
@@ -35,15 +33,13 @@ namespace thunder
 					start_found = s_.find( start_marker, end_found );
 					end_found = s_.find( end_marker, start_found + 1 );
 
-					if ( start_found == std::string::npos || end_found == std::string::npos )
-					{
+					if ( ( start_found == std::string::npos ) || ( end_found == std::string::npos ) )
 						break;
-					}
 
 					result.push_back( s_.substr( start_found + start_marker.length(), end_found - start_found - ( end_marker.length() + 1 ) ) );
 					end_found++;
 
-				} while ( start_found != std::string::npos && end_found != std::string::npos );
+				} while ( ( start_found != std::string::npos ) && ( end_found != std::string::npos ) );
 
 				return std::move( result );
 			}
