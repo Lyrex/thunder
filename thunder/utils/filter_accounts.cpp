5089c587f05bb42d2be2f14ce54504b86e9b360e
Lyrex <admin@lyrex.net>
2017-09-02 16:29:40 +0200

    Add second example

---
+++ thunder/utils/filter_accounts.cpp
@@ -0,0 +1,65 @@
+/*
+
+#include <iostream>
+#include <chrono>
+#include <fstream>
+#include <vector>
+
+#if _HAS_CXX17
+#include <string_view>
+#else
+#include <string>
+#endif
+
+#define FMT_HEADER_ONLY
+
+#include <fmt/format.h>
+
+#include <thunder/utils/string.hpp>
+
+int not_main(int argc, char* argv[])
+{
+	std::ifstream account_sql{ "C:\\undergroundmt2_account.sql", std::ios::in | std::ios::binary };
+	std::ofstream filtered_account_sql{ "C:\\undergroundmt2_account_filtered.sql", std::ios::out | std::ios::binary };
+	if (!account_sql.is_open())
+		return 1;
+
+	// for a bit of efficiency
+	account_sql.seekg(0, account_sql.end);
+	const auto len = account_sql.tellg();
+	account_sql.seekg(0, account_sql.beg);
+
+	std::vector<char> data_vec(len);
+	account_sql.read(data_vec.data(), len);
+
+	auto lines = thunder::utils::string::split(data_vec.data(), "\r\n");
+
+	fmt::MemoryWriter w;
+
+	for (auto&& line: lines)
+	{
+		auto query_data = thunder::utils::string::between(line, "INSERT INTO account VALUES (", ");");
+		if (query_data.size() < 1)
+			continue;
+		
+		auto split_data = thunder::utils::string::between(query_data[0], "'", "', ", false, false);
+		
+		if (split_data.size() < 6)
+			continue;
+
+		w.write("INSERT INTO account (id, login, password, email) VALUES ('{}', '{}', '{}', '{}');\n", std::string(split_data[0]), std::string(split_data[1]), std::string(split_data[2]), std::string(split_data[5]));
+	}
+
+	filtered_account_sql << w.c_str();
+	filtered_account_sql.flush();
+
+	filtered_account_sql.close();
+
+	std::cout << "finished. press enter to close" << std::endl;
+
+	std::cin.get();
+	
+	return 0;
+}
+
+*/
